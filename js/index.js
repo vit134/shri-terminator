4c5f3f9ee28a821a460eb73bf4f23cd57b40cb4a
vit134 <vit134@mail.ru>
2018-03-15 10:50:59 +0300

    сделал что то непонятное нужно поработать с другого компа

--- js/index.js
+++ js/index.js
@@ -7,6 +7,12 @@ var video = document.querySelector('video'),
     context = canvas.getContext('2d'),
     videoStream = null;
 
+context.globalCompositeOperation = 'source-out';
+
+
+var canvasLayerVideo = 'video',
+    canvasLayerVideo = 'interface';
+
 var tracker = new tracking.ObjectTracker("face");
     tracker.setInitialScale(4);
     tracker.setStepSize(2);
@@ -18,16 +24,20 @@ tracker.on('track', function(event) {
     //console.log('tracker track');
     context.clearRect(0, 0, canvas.width, canvas.height);
     //console.log(canvas.width, canvas.height);
-    event.data.forEach(function(rect) {
-        context.strokeStyle = '#a64ceb';
-        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-        context.font = '11px Helvetica';
-        context.fillStyle = "#fff";
-        //context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-        //context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-        context.fillText('x: ' + rect.x + 'px', 60, 2git 0);
-        context.fillText('y: ' + rect.y + 'px', 60, 40);
-    });
+    if (event.data) {
+        event.data.forEach(function(rect) {
+            console.log(rect.x);
+            console.log(rect.y);
+            context.strokeStyle = '#a64ceb';
+            context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+            context.font = '11px Helvetica';
+            context.fillStyle = "#fff";
+            //context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+            //context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+            context.fillText('x: ' + rect.x + 'px', 60, 20);
+            context.fillText('y: ' + rect.y + 'px', 60, 40);
+        });
+    }
 });
 
 
@@ -47,6 +57,7 @@ function bindEvents() {
     pauseBtn.addEventListener('click', _pauseCapture);
 
     video.addEventListener('canplay', _videoOnCanPlay);
+    video.addEventListener('canplaythrough', _videOnLoad)
 }
 
 function _videoOnCanPlay() {
@@ -54,21 +65,22 @@ function _videoOnCanPlay() {
     video.classList.remove('hidden');
 }
 
+function _videOnLoad() {
+    draw(video, context);
+}
+
 function _startCapture() {
     preloader.classList.remove('hidden');
 
     if (navigator.getUserMedia) {
-        navigator.getUserMedia({ audio: true, video: {} },
-            function(stream) {
-                video.srcObject = videoStream = stream;
+        navigator.mediaDevices.getUserMedia({audio: true, video: {width: 700, height: 350}})
+            .then(function(mediaStream) {
+                video.srcObject = mediaStream;
                 video.onloadedmetadata = function(e) {
                     video.play();
                 };
-            },
-            function(err) {
-                console.log("The following error occurred: " + err.name);
-            }
-        );
+            })
+            .catch(function(err) { console.log(err.name + ": " + err.message); });
     } else {
         console.log("getUserMedia not supported");
     }
@@ -77,6 +89,13 @@ function _startCapture() {
 function _pauseCapture() {
     video.pause();
 
+}
+
+function draw(video) {
+    context.drawImage(video, 0, 0, 700, 350);
+    requestAnimationFrame(() => {
+        draw(video);
+});
 }
 
 function checkGetUserMedia() {
