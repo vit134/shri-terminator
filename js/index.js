ac328f6b4836cc2bba269fa9ac03176d92d054c4
vit134 <vit134@mail.ru>
2018-03-16 00:42:50 +0300

    добавил кнопку для запуска отслеживания лица

--- js/index.js
+++ js/index.js
@@ -1,50 +1,85 @@
 var startBtn = document.querySelector('.js-btn-start'),
     pauseBtn = document.querySelector('.js-btn-pause'),
+    faseBtn = document.querySelector('.js-btn-face'),
     preloader = document.querySelector('.video-preloader');
 
 var video = document.querySelector('video'),
     canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d');
 
+var svg = document.querySelector('#js-faces-targets')
+
 var ostAudio = document.querySelector('#ost'),
     shagiAudio = document.querySelector('#ost');
-
-context.globalCompositeOperation = 'source-out';
 
 var tracker = new tracking.ObjectTracker("face");
     tracker.setInitialScale(4);
     tracker.setStepSize(2);
     tracker.setEdgesDensity(0.1);
 
-//tracking.track('video', tracker, { camera: true });
-
-tracker.on('track', function(event) {
-    //console.log('tracker track');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    //console.log(canvas.width, canvas.height);
-    if (event.data) {
-        event.data.forEach(function(rect) {
-            console.log(rect.x);
-            console.log(rect.y);
-            context.strokeStyle = '#a64ceb';
-            context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-            context.font = '11px Helvetica';
-            context.fillStyle = "#fff";
-            //context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-            //context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-            context.fillText('x: ' + rect.x + 'px', 60, 20);
-            context.fillText('y: ' + rect.y + 'px', 60, 40);
-        });
-    }
-});
-
-
 function init() {
     checkGetUserMedia();
     bindEvents();
-    generateStroke();
     createAudio();
 
+    _startCapture()
+}
+
+function bindEvents() {
+    startBtn.addEventListener('click', _startCapture);
+    pauseBtn.addEventListener('click', _pauseCapture);
+    faseBtn.addEventListener('click', function () {
+        if (!this.classList.contains('started')) {
+            this.classList.add('started');
+            tracking.track('#video', tracker, {camera: true});
+        } else {
+            this.classList.remove('started');
+            //tracking.removeAllListeners(tracker);
+            console.log(tracker, tracking);
+        }
+    });
+
+    video.addEventListener('canplay', _videoOnCanPlay);
+    video.addEventListener('canplaythrough', _videOnLoad);
+
+    tracker.on('track', function(event) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        if (event.data) {
+            event.data.forEach(function(rect) {
+                /*console.log(rect.x);
+                console.log(rect.y);*/
+                context.strokeStyle = 'white';
+                context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+                context.font = '11px Helvetica';
+                context.fillStyle = "#fff";
+                //context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+                //context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+                context.fillText('x: ' + rect.x + 'px', 60, 20);
+                context.fillText('y: ' + rect.y + 'px', 60, 40);
+
+                var face = svg.querySelector('#face');
+
+                var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+                var textX = document.createElementNS("http://www.w3.org/2000/svg", "text");
+                var textY = document.createElementNS("http://www.w3.org/2000/svg", "text");
+
+                /*text.setAttribute('x', 0)
+
+                text.setAttribute('y', i * 20)
+
+                text.textContent = i + ' - ' + makeRandomString();
+
+                g.appendChild(text);
+
+                digits.appendChild(g)*/
+            });
+        }
+    });
+}
+
+function createInterface() {
+    generateStroke();
 
     setInterval(function () {
         document.querySelector('#js-faces-targets').classList.add('noise');
@@ -52,98 +87,86 @@ function init() {
         var t = setTimeout(function () {
             document.querySelector('#js-faces-targets').classList.remove('noise');
         }, 3000)
-    }, 5000)
-}
-
-function bindEvents() {
-    startBtn.addEventListener('click', _startCapture);
-    pauseBtn.addEventListener('click', _pauseCapture);
-
-    video.addEventListener('canplay', _videoOnCanPlay);
-    video.addEventListener('canplaythrough', _videOnLoad)
+    }, 10000)
 }
 
 function generateStroke() {
     for (let i = 0; i <= 10; i++) {
-        var svg = document.querySelector('#js-faces-targets')
+        var digits = svg.querySelector('#digits');
         var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
         var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
 
-        text.setAttribute('x', 30)
-        text.setAttribute('y', i * 30)
-        text.textContent = i + 1 + makeRandomString();
+        text.setAttribute('x', 0)
+
+        text.setAttribute('y', i * 20)
+
+        text.textContent = i + ' - ' + makeRandomString();
 
         g.appendChild(text);
 
-        svg.appendChild(g)
+        digits.appendChild(g)
     }
 }
 
 function createAudio() {
+    var context = new window.AudioContext();
+    var buffers = [], sources=[], destination;
 
-        var context = new window.AudioContext();
-        var buffers = [], sources=[], destination;
+    var stop = function(index){
+        sources[0].stop(0);
+        sources[1].stop(1);
+    }
 
-        var stop = function(index){
-            sources[0].stop(0);
-            sources[1].stop(1);
-        }
+    var play = function(index){
+        sources = [context.createBufferSource(), context.createBufferSource()];
 
-        var play = function(index){
-            sources = [context.createBufferSource(), context.createBufferSource()];
+        sources[0].buffer = buffers[0];
+        sources[1].buffer = buffers[1];
 
-            sources[0].buffer = buffers[0];
-            sources[1].buffer = buffers[1];
+        destination = context.destination;
 
-            destination = context.destination;
+        gainNodes = [context.createGain(),context.createGain()];
 
-            gainNodes = [context.createGain(),context.createGain()];
+        gainNodes[0].gain.value  = 10;
+        gainNodes[1].gain.value  = 5;
 
-            gainNodes[0].gain.value  = 10;
-            gainNodes[1].gain.value  = 5;
+        sources[0].connect(gainNodes[0]);
+        sources[1].connect(gainNodes[1]);
 
-            sources[0].connect(gainNodes[0]);
-            sources[1].connect(gainNodes[1]);
+        gainNodes[0].connect(destination);
+        gainNodes[1].connect(destination);
 
-            gainNodes[0].connect(destination);
-            gainNodes[1].connect(destination);
-            
-            console.log(sources[0]);
-            console.log(destination);
-            
-            sources[0].start(0);
-            sources[1].start(0);
-        }
+        console.log(sources[0]);
+        console.log(destination);
 
-        var initSound = function(arrayBuffer, bufferIndex) {
-            context.decodeAudioData(arrayBuffer, function(decodedArrayBuffer) {
-                buffers[bufferIndex] = decodedArrayBuffer;
-                if(buffers[0] && buffers[1]) document.querySelector('body').classList.add('readytoplay');
-            }, function(e) {
-                console.log('Error decoding file', e);
-            });
-        }
+        sources[0].start(0);
+        sources[1].start(0);
+    }
 
-        var loadSoundFile = function(url, bufferIndex) {
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', url, true);
-            xhr.responseType = 'arraybuffer';
-            xhr.onload = function(e) {
-                initSound(this.response, bufferIndex); // this.response is an ArrayBuffer.
-            };
-            xhr.send();
-        }
+    var initSound = function(arrayBuffer, bufferIndex) {
+        context.decodeAudioData(arrayBuffer, function(decodedArrayBuffer) {
+            buffers[bufferIndex] = decodedArrayBuffer;
+            if(buffers[0] && buffers[1]) document.querySelector('body').classList.add('readytoplay');
+        }, function(e) {
+            console.log('Error decoding file', e);
+        });
+    }
 
-        loadSoundFile('ost.mp3',0);
-        loadSoundFile('shagi_po_zemle.mp3',1);
+    var loadSoundFile = function(url, bufferIndex) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'arraybuffer';
+        xhr.onload = function(e) {
+            initSound(this.response, bufferIndex); // this.response is an ArrayBuffer.
+        };
+        xhr.send();
+    }
+
+    loadSoundFile('ost.mp3',0);
+    loadSoundFile('shagi_po_zemle.mp3',1);
 
 
-        play();
-        /*$('.range1').on('change', function(e){
-            if(!gainNodes) return;
-            gainNodes[0].gain.value  = 1 - $('.range1').val();
-            gainNodes[1].gain.value  = $('.range1').val();
-        });*/
+    play();
 }
 
 function _videoOnCanPlay() {
@@ -154,29 +177,31 @@ function _videoOnCanPlay() {
 function _videOnLoad() {
     console.log('onload');
     video.play();
-    createInterface()
     //draw(video, context);
 }
 
 function _startCapture() {
     preloader.classList.remove('hidden');
 
-    /*if (navigator.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({audio: true, video: {width: 700, height: 350}})
+    if (navigator.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({audio: true, video: true})
             .then(function(mediaStream) {
                 video.srcObject = mediaStream;
+                createInterface();
+
                 video.onloadedmetadata = function(e) {
                     video.play();
+
                 };
             })
             .catch(function(err) {
                 console.log(err);
                 console.log(err.name + ": " + err.message);
-                videoFallBack()
+                videoFallBack();
             });
     } else {
         console.log("getUserMedia not supported");
-    }*/
+    }
 }
 
 function videoFallBack() {
